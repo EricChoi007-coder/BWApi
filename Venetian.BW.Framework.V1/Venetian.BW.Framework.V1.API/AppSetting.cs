c1fd246bcaf7b517b23686f24d20c6f50311bdbe
DESKTOP-BGNLLQO\Administrator <562127596@qq.com>
2022-03-17 17:51:33 +0800

    first commit

---
+++ Venetian.BW.Framework.V1/Venetian.BW.Framework.V1.API/AppSetting.cs
@@ -0,0 +1,9 @@
+﻿namespace Venetian.BW.Framework.V1.API
+{
+    public class AppSetting
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string DbConnection { get; set; }
+    }
+}
